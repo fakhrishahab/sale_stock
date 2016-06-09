701f34a1ea39bf8077224fb19022a5590d4ed3bf
fakhrishahab <fakhri1804@gmail.com>
2016-06-09 16:05:31 +0700

    update

--- app_api.js
+++ app_api.js
@@ -7,26 +7,30 @@ var express 	= require('express'),
 
 var product = db.collection('product');
 
-product.insert([
-	{name: 'Frindha Pointy Glitter Heels', price : 189.000, size: [36, 37, 38, 39], image: './assets/images/product/1.jpg'},
-	{name: 'Velicia Stripes Bodycon Midi Dress', price : 89.000, size: ['S'], image: './assets/images/product/2.jpg'},
-	{name: 'Ferrona Plain Sporty Backpack', price : 159.000, size: [], image: './assets/images/product/3.jpg'},
-	{name: 'Zerszina Plain Tunic Blouse', price : 109.000, size: ['L'], image: './assets/images/product/4.jpg'},
-	{name: 'Louisfoxel Leather High Boots', price : 199.000, size: [36, 37, 38, 39], image: './assets/images/product/5.jpg'},
-	{name: 'Fristiania Text Longsleeve Sweater', price : 109.000, size: ['XL'], image: './assets/images/product/6.jpg'},
-	{name: 'Rhistyza Polkadot Sleeveless Shirt', price : 83.000, size: ['L'], image: './assets/images/product/7.jpg'},
-	{name: 'Delvea Plain Pleated Midi Dress', price : 89.000, size: ['L'], image: './assets/images/product/8.jpg'},
-])
+// product.insert([
+// 	{name: 'Frindha Pointy Glitter Heels', price : 189.000, size: [36, 37, 38, 39], image: './assets/images/product/1.jpg'},
+// 	{name: 'Velicia Stripes Bodycon Midi Dress', price : 89.000, size: ['S'], image: './assets/images/product/2.jpg'},
+// 	{name: 'Ferrona Plain Sporty Backpack', price : 159.000, size: [], image: './assets/images/product/3.jpg'},
+// 	{name: 'Zerszina Plain Tunic Blouse', price : 109.000, size: ['L'], image: './assets/images/product/4.jpg'},
+// 	{name: 'Louisfoxel Leather High Boots', price : 199.000, size: [36, 37, 38, 39], image: './assets/images/product/5.jpg'},
+// 	{name: 'Fristiania Text Longsleeve Sweater', price : 109.000, size: ['XL'], image: './assets/images/product/6.jpg'},
+// 	{name: 'Rhistyza Polkadot Sleeveless Shirt', price : 83.000, size: ['L'], image: './assets/images/product/7.jpg'},
+// 	{name: 'Delvea Plain Pleated Midi Dress', price : 89.000, size: ['L'], image: './assets/images/product/8.jpg'},
+// ])
 
 app
 
 .get('/api/product/get_all', function(req, res){
-	res.send(product.items)
+	res.json(product.items)
 })
 
 .get('/api/product/get/:id', function(req, res){
 	res.send(product.get(parseInt(req.params.id)))
 	console.log(product.get(0))
+})
+
+.delete('/api/product/delete/:id', function(req, res){
+	console.log()
 })
 
 app.use('/adm*n', admin);
--- app_routing.js
+++ app_routing.js
@@ -1,8 +1,16 @@
 var express 	= require('express'),
+	request 	= require('request'),
 	route		= express.Router();
 
 route.get('/', function(req, res){
-	res.render('index')
+	var products;
+	request('http://localhost:3000/api/product/get_all', function(error, response, body){
+		products = body;
+		res.render('index',{
+			product : products
+		})
+	})
+	
 })
 
 module.exports = route;
